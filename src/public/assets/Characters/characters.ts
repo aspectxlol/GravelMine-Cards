8f4645f2cc93606876043cab90c41561504b8731
aspectx <gamernxt6@gmail.com>
2022-03-27 16:12:38 +0700

    updated skins & added aspectx skin

--- src/public/assets/Characters/characters.ts
+++ src/public/assets/Characters/characters.ts
@@ -1,13 +1,20 @@
+import { PathLike } from 'fs';
 import path from 'path';
 
 type username = 'EmirHantr3' | 'KingDooms' | 'WinLogon' | 'Aspectx' | 'BigGaben' | 'Was_Machin' | 'oCerial' | 'KingLucasThe1st' | 'Addiblue126' | 'iHassani' | 'Cjp' | 'Wtcnn' | 'Lax Majo / Lx MJO' | 'BotiestBot2' | 'KittyClip' | 'BubblesTheKitten' | 'EvilGaming_yt' | 'GRAVELMINE' | 'Pokduc' | 'JuliusE2010' | 'aTree' | 'pgc' | 'Alexisco_YT' | 'TiagoMasterGamer' | 'anim1311'
 type character = {
     username: username,
-    pathToSkin: string,
-    pathToAvatar: string
+    pathToSkin: PathLike | string,
+    pathToAvatar: PathLike | string
 }
 
-const character: character[] = [] //On Skin Add, push Character to this Array
+const character: character[] = [
+    {
+        username: 'Aspectx',
+        pathToAvatar: 'Not Defined',
+        pathToSkin: '@Skin/Aspectx.png'
+    }
+] //On Skin Add, push Character to this Array
 
 //Schema 
 // {
@@ -15,7 +22,7 @@ const character: character[] = [] //On Skin Add, push Character to this Array
 //     pathToSkin: path.join(__dirname + '/skins/username.png')    
 // }
 
-//Minotar Api to get Skins https://minotar.net/bust/username/100.png
+//Minotar Api to get Skins https://minotar.net/armor/bust/fcb600a/100.png
 
 export function getCharacterByUsername(username: username):character | undefined{
     const foundCharacter: character | undefined = character.find(characters => characters.username == username)

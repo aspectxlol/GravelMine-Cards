e2923528b5c8499d1c5fd4816de2c5b533ef97ae
aspectx <gamernxt6@gmail.com>
2022-05-12 18:37:54 +0700

    clean

--- src/public/assets/Badges/Badges.ts
+++
@@ -1,8 +0,0 @@
-import { Roles, Badges } from "src/config/config"
-
-const badges: Badges[] = []
-
-export function getBadgeByRole(Role: Roles):Badges | undefined{
-    const foundCharacter: Badges | undefined = badges.find(badge => badge.Role = Role )
-    return foundCharacter
-}
--- src/public/assets/background/Background.ts
+++
@@ -1,8 +0,0 @@
-import { Rarity, Backgrounds } from "src/config/config"
-
-const background: Backgrounds[] = []
-
-export function getBackgroundByRarity(Rarity: Rarity): Backgrounds | undefined {
-    const foundBackground = background.find(x => x.Rarity == Rarity)
-    return foundBackground;
-}
